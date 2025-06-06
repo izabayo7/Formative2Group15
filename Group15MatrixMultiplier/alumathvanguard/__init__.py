bafffce7b7af0b57a25b54aedb2186bdb0474e1f
⁠izabayo7 <cedricizabayo7@gmail.com>
2025-06-06 14:47:28 +0200

    chore: export the MatrixMultiplicationError

--- Group15MatrixMultiplier/alumathvanguard/__init__.py
+++ Group15MatrixMultiplier/alumathvanguard/__init__.py
@@ -11,10 +11,10 @@ Authors:
 Version: 1.0.0
 """
 
-from .matrix_ops import MatrixMultiplier, Matrix, multiply_matrices, create_matrix
+from .matrix_ops import MatrixMultiplier, Matrix, multiply_matrices, create_matrix, MatrixMultiplicationError
 
 __version__ = "1.0.0"
 __author__ = "ALU Math Vanguard Group 15"
 
 # Make key classes available at package level
-__all__ = ['MatrixMultiplier', 'Matrix', 'multiply_matrices', 'create_matrix']
+__all__ = ['MatrixMultiplier', 'Matrix', 'multiply_matrices', 'create_matrix', 'MatrixMultiplicationError']
